55c11bf077e442f57e4ecfd4b6729a611cd27bbf
tarde <tarde@AulaFP>
2023-04-10 16:20:07 +0200

    Viva, un commit en local

---
+++ Entornos_Depuracion/src/depuracion/Dimitry_Tarea5.java
@@ -0,0 +1,16 @@
+package depuracion;
+
+public class Dimitry_Tarea5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String frase = """
+				Esto es un bloque de texto
+				Para la prueba de Entornos
+				Por Dmitry Poldnev
+				""";
+		System.out.println(frase);
+	}
+
+}
