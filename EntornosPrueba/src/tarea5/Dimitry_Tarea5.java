8ddcb6a799c4694f412cbb277d67ff8e22e96883
tarde <tarde@AulaFP>
2023-04-17 16:08:39 +0200

    Hola mundo

---
+++ EntornosPrueba/src/tarea5/Dimitry_Tarea5.java
@@ -0,0 +1,12 @@
+package tarea5;
+
+public class Dimitry_Tarea5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String frase="Hello World";
+		System.out.println(frase);
+	}
+
+}
